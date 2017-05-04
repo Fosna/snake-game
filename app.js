efb772625b9c1da30a48cbb75fec7fbbd0162b72
Vedran Marsic <vedran.marsic@axilis.com>
2017-05-04 08:13:53 +0200

    Paint the snake.

--- app.js
+++ app.js
@@ -5,11 +5,29 @@ document.ready().then(function () {
     const h = 450;
 
     // Paint empty canvas with border.
-    
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, w, h);
     ctx.strokeStyle = "black";
     ctx.strokeRect(0, 0, w, h);
 
     console.log("monkey");
+
+    // Create snake model
+    // Array of snake cells.
+    const snake_array = [];
+
+    // Length of the snake
+    const length = 5;
+    for(let i = 0; i < length - 1; i++) {
+        snake_array.push({x: i, y: 0});
+    }
+
+    // Paint the snake
+    for(let i = 0; i < snake_array.length; i++) {
+        const cell = snake_array[i];
+        
+        // Paint cell
+        ctx.fillStyle = "blue";
+        ctx.fillRect(cell.x * 10, cell.y * 10, 9, 9);
+    }
 });
