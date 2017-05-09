8872a2e3a89f4239b0d7dcf30f22d2b925a30d26
Vedran Marsic <vedran.marsic@axilis.com>
2017-05-09 09:33:01 +0200

    Snake reacts to user input.

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 class SnakeGame {
     constructor() {
         this.graphics = new Graphics("canvas");
-        
+
         this.snake = new SnakeModel(2);
 
         this.userControlls = new UserControlls();
@@ -14,19 +14,23 @@ class SnakeGame {
     }
 
     tick() {
-        console.log(this.userControlls.direction);
+        this
+            .snake
+            .move(this.userControlls.direction);
 
-        this.snake.move();
-
-        this.graphics.paintEmptyCanvas();
-        this.graphics.paintSnake(this.snake);
+        this
+            .graphics
+            .paintEmptyCanvas();
+        this
+            .graphics
+            .paintSnake(this.snake);
     }
 }
 
 class Graphics {
     constructor(canvasId) {
         const canvas = document.getElementById(canvasId);
-        
+
         this.ctx = canvas.getContext("2d");
         this.w = canvas.width;
         this.h = canvas.height;
@@ -34,15 +38,21 @@ class Graphics {
 
     paintEmptyCanvas() {
         this.ctx.fillStyle = "white";
-        this.ctx.fillRect(0, 0, this.w, this.h);
+        this
+            .ctx
+            .fillRect(0, 0, this.w, this.h);
         this.ctx.storkeStyle = "black";
-        this.ctx.strokeRect(0, 0, this.w, this.h);
+        this
+            .ctx
+            .strokeRect(0, 0, this.w, this.h);
     }
 
     paintSnake(snake) {
-        for(let cell of snake.snakeArray) {
+        for (let cell of snake.snakeArray) {
             this.ctx.fillStyle = "blue";
-            this.ctx.fillRect(cell.x * 10, cell.y * 10, 9, 9);
+            this
+                .ctx
+                .fillRect(cell.x * 10, cell.y * 10, 9, 9);
         }
     }
 }
@@ -50,19 +60,40 @@ class Graphics {
 class SnakeModel {
     constructor(length) {
         this.snakeArray = [];
-        for(let i = 0; i < length; i++) {
-            this.snakeArray.push({x: i, y: 0});
+        for (let i = 0; i < length; i++) {
+            this
+                .snakeArray
+                .push({x: i, y: 0});
         }
     }
 
-    move() {
+    move(direction) {
         const head = this.snakeArray[this.snakeArray.length - 1];
-        const newHead = { x: head.x + 1, y: head.y };
+
+        let x = head.x;
+        let y = head.y;
+        if (direction === "up") {
+            y--;
+        } else if (direction === "right") {
+            x++;
+        } else if (direction === "down") {
+            y++;
+        } else if (direction === "left") {
+            x--;
+        }
+        const newHead = {
+            x,
+            y
+        };
 
         // Add new head.
-        this.snakeArray.push(newHead);
+        this
+            .snakeArray
+            .push(newHead);
         // Remove tail.
-        this.snakeArray.shift(0);
+        this
+            .snakeArray
+            .shift(0);
     }
 }
 
@@ -86,4 +117,3 @@ const KeyToDirection = {
     39: "right",
     40: "down"
 }
-
